4d85f1eea60c22e78721d7cd6617a39d98fb762d
rpytel1 <rafal.pytel1@wp.pl>
2018-12-07 14:54:46 +0100

    Adding receiveInitiate

--- Assignment3/src/assignment3/node/Node.java
+++ Assignment3/src/assignment3/node/Node.java
@@ -13,7 +13,7 @@ import java.util.Queue;
 import java.util.Set;
 import java.util.TreeSet;
 
-public class Node extends UnicastRemoteObject implements IComponent{
+public class Node extends UnicastRemoteObject implements IComponent {
 
     private Node[] nodes;
 
@@ -23,9 +23,13 @@ public class Node extends UnicastRemoteObject implements IComponent{
     private double fragmentName;
 
     private Queue<Link> coreLinks;
-    private int weightBestAdjecent;
     private Queue<Link> links;
     private Queue<Link> moeCandidatesList;
+
+    private int weightBestAdjecent;
+    private Link bestEdge;
+    private Link testEdge;
+    private Link inBranch;
     public int findCount;
 
     protected Node(int id, Set<Link> links) throws RemoteException {
@@ -38,17 +42,17 @@ public class Node extends UnicastRemoteObject implements IComponent{
     }
 
     public void wakeUp() throws RemoteException {
-    	Link edge = links.peek();
-    	edge.setState(LinkState.IN_MST); // TODO: to be checked 
-    	level = 0;
-    	state = NodeState.FOUND;
+        Link edge = links.peek();
+        edge.setState(LinkState.IN_MST); // TODO: to be checked
+        level = 0;
+        state = NodeState.FOUND;
         findCount = 0;
-        Message msg = new Message(MessageType.CONNECT,0);
+        Message msg = new Message(MessageType.CONNECT, 0);
         nodes[edge.getReceiver(id)].receive(msg, edge);
     }
 
 
-    public void receive(Message message, Link link)  throws RemoteException {
+    public void receive(Message message, Link link) throws RemoteException {
         switch (message.getType()) {
             case TEST:
                 receiveTest(message, link);
@@ -73,32 +77,50 @@ public class Node extends UnicastRemoteObject implements IComponent{
     }
 
     private void receiveConnect(Message message, Link link) throws RemoteException {
-    	if (state == NodeState.SLEEPING)
-    		wakeUp();
-    	if (message.getLevel()<this.level){
-    		link.setState(LinkState.IN_MST); // TODO: to be checked
-    		Message msg = new Message(MessageType.INITIATE,level,fragmentName,state);
-    		nodes[link.getReceiver(id)].receive(msg, link);
-    		if (state == NodeState.FIND)
-    			findCount++;
-    	}
-		else{
-			if (link.getState() == LinkState.CANDIDATE_IN_MST){
-				// TODO: append message to the message queue
-			}
-			else{
-				Message msg = new Message(MessageType.INITIATE,level+1,link.getWeight(),NodeState.FIND);
-	    		nodes[link.getReceiver(id)].receive(msg, link);
-			}
-		}
+        if (state == NodeState.SLEEPING)
+            wakeUp();
+        if (message.getLevel() < this.level) {
+            link.setState(LinkState.IN_MST); // TODO: to be checked
+            Message msg = new Message(MessageType.INITIATE, level, fragmentName, state);
+            nodes[link.getReceiver(id)].receive(msg, link);
+            if (state == NodeState.FIND)
+                findCount++;
+        } else {
+            if (link.getState() == LinkState.CANDIDATE_IN_MST) {
+                // TODO: append message to the message queue
+            } else {
+                Message msg = new Message(MessageType.INITIATE, level + 1, link.getWeight(), NodeState.FIND);
+                nodes[link.getReceiver(id)].receive(msg, link);
+            }
+        }
     }
 
-    private void receiveInitiate(Message message, Link link) {
+    private void receiveInitiate(Message message, Link link) throws RemoteException {
+        level = message.getLevel();
+        fragmentName = message.getfName();
+        state = message.getSenderState();
+        inBranch = link;
+        bestEdge = null;//TODO: NIL
+        weightBestAdjecent = -10000; // - INFINITY
 
+        for (Link adjescentLink : this.links) {//TODO: Not sure if this is the correct one list of links
+            if (adjescentLink != link && adjescentLink.getState() == LinkState.IN_MST) {
+                Message msg = new Message(MessageType.INITIATE, level, fragmentName, state);
+                nodes[adjescentLink.getReceiver(id)].receive(msg, adjescentLink);
+                if (state == NodeState.FIND) {
+                    findCount = findCount + 1;
+                }
+            }
+        }
+        if (state == NodeState.FIND) {
+            test();
+        }
     }
 
     private void test() {
+        if(links.stream().anyMatch(p->p.getState()==LinkState.CANDIDATE_IN_MST)){//TODO: check if adjecent links are the ones
 
+        }
     }
 
     private void receiveTest(Message message, Link link) {
